60f96c612f6ad61372a7b0130f8c21559a5b6719
Golosoman <constan.lapin2015@mail.ru>
2023-09-16 16:23:14 +0400

    add method find_movie_genre and some comments and notation by functions

--- app/movie/view.py
+++ app/movie/view.py
@@ -15,12 +15,20 @@ def movie_title_page(title):
     return jsonify(movie)
 
 
+# Функция загрузки страницы c json по отрезку годов
 @movie_blueprint.route("/movie/<first_year>/to/<second_year>")
 def movie_year_to_year_page(first_year, second_year):
     movies = movies_dao.find_movies_by_year_to_year(first_year, second_year)
     return jsonify(movies)
 
+# Функция загрузки страницы c json по возрастному ограничению
 @movie_blueprint.route("/rating/<value_rating>")
 def movie_by_rating_page(value_rating):
     movies = movies_dao.find_movies_by_rating(value_rating)
+    return jsonify(movies)
+
+# Функция загрузки страницы c json по определенному жанру
+@movie_blueprint.route("/genre/<genre_value>")
+def movie_by_genre_page(genre_value):
+    movies = movies_dao.find_films_by_genre(genre_value)
     return jsonify(movies)
